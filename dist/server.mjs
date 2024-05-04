c9c4b3c184435143ff3ebbf3b997a61d6a142c03
Arthur Levy <arthurlbvieiraa@gmail.com>
2024-05-04 00:29:44 -0300

    feat: added swagger ui

--- dist/server.mjs
+++ dist/server.mjs
@@ -30,7 +30,9 @@ import {
 // src/server.ts
 import fastify from "fastify";
 import { fastifyCors } from "@fastify/cors";
-import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
+import { serializerCompiler, validatorCompiler, jsonSchemaTransform } from "fastify-type-provider-zod";
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUi from "@fastify/swagger-ui";
 var require_server = __commonJS({
   "src/server.ts"(exports) {
     init_get_all_events();
@@ -42,6 +44,21 @@ var require_server = __commonJS({
     var app = fastify();
     app.register(fastifyCors, {
       origin: "*"
+    });
+    app.register(fastifySwagger, {
+      swagger: {
+        consumes: ["application/json"],
+        produces: ["application/json"],
+        info: {
+          title: "pass.in",
+          description: "Especifica\xE7\xF5es da API Pass.in",
+          version: "1.0"
+        }
+      },
+      transform: jsonSchemaTransform
+    });
+    app.register(fastifySwaggerUi, {
+      routePrefix: "/docs"
     });
     app.setSerializerCompiler(serializerCompiler);
     app.setValidatorCompiler(validatorCompiler);
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 // Dependencies
 import fastify from 'fastify';
 import { fastifyCors } from '@fastify/cors';
-import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod';
+import { serializerCompiler, validatorCompiler, jsonSchemaTransform } from 'fastify-type-provider-zod';
 import { prisma } from './lib/prisma';
+import fastifySwagger from '@fastify/swagger';
+import fastifySwaggerUi from '@fastify/swagger-ui';
 
 // Routes
 import { getAllEvents } from './routes/get-all-events';
@@ -12,10 +14,28 @@ import { login } from './routes/login';
 import { createRegister } from './routes/create-register';
 import { registerUserOnEvent } from './routes/register-user-on-event';
 
+
 const app = fastify();
 
 app.register(fastifyCors, {
 	origin: '*'
+})
+
+app.register(fastifySwagger, {
+	swagger: {
+		consumes: ['application/json'],
+		produces: ['application/json'],
+		info: {
+			title: 'pass.in',
+			description: 'Especificações da API Pass.in',
+			version: '1.0'
+		},
+	},
+	transform: jsonSchemaTransform
+});
+
+app.register(fastifySwaggerUi, {
+	routePrefix: '/docs'
 })
 
 app.setSerializerCompiler(serializerCompiler);
